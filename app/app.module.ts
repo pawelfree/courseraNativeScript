88f0d736366757575168638c6cfed4d2c115337d
Pawel Dudek <pawelfree@wp.pl>
2018-05-06 17:05:14 +0200

    Buttons

--- app/app.module.ts
+++ app/app.module.ts
@@ -17,6 +17,7 @@ import { ContactComponent } from './contact/contact.component';
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
+import { FavoriteService } from './services/favorite.service';
 
 import { baseURL } from './shared/baseurl';
 // Uncomment and add to NgModule imports if you need to use two-way binding
@@ -52,6 +53,7 @@ import { baseURL } from './shared/baseurl';
         DishService,
         LeaderService,
         PromotionService,
+        FavoriteService,
         ProcessHTTPMsgService
     ],
     schemas: [
